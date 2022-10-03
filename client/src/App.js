7736ffdd44704668d8a221333e2ca1c744d3b77d
Kate Danilova <ecdanilova@gmail.com>
2022-10-03 19:19:42 -0400

    create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
-const App = () => {
-    return (
-      <h1>Hello World</h1>
-    );
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
 
+
+const App = () => {
+
+  return(
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <HomePage/> } />
+        <Route path="login" element={ <h1>LogIn</h1> } />
+        <Route path="signup" element={ <h1>SignUp</h1> } />
+        <Route path="search" element={ <h1>SearchPage</h1> } />
+        <Route path="search/detail" element={ <h1>DetailPage</h1> } />
+        <Route path="profile" element={ <h1>ProfilePage</h1> } />
+        <Route path="recipe" element={ <h1>AddRecipePage</h1> } />
+        <Route path="recipe/detail" element={ <h1>DetailRecipePage</h1> } />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 export default App;
+
---
+++ client/src/HomePage.js
@@ -0,0 +1,7 @@
+const HomePage = () => {
+    return(
+        <h1>Welcome content here</h1>
+    )
+}
+
+export default HomePage;
