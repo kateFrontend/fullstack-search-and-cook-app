d518df25f45e004ed28833ebe1a8db3712a20e9e
kateFrontend <ecdanilova@gmail.com>
2022-10-05 15:58:51 -0400

    update styles and add loader

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import './style.css'
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
