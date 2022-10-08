1c772e138ea343d872cd36288a60b172fd8e0a84
kateFrontend <ecdanilova@gmail.com>
2022-10-08 18:48:44 -0400

    update home page

---
+++ client/src/components/Services.js
@@ -0,0 +1,36 @@
+
+
+const Services = () => {
+  return (
+    <div>
+      <h2 className='section-title'>our services</h2>
+    <section className='section el-section'>
+
+      <div className="element">
+<div>
+<h3 className="el-title">Search Recipes</h3>
+        <p className="el-descr">sdfsdfsdfsdf</p>
+</div>
+      </div>
+
+      <div className="element">
+<div>
+<h3 className="el-title">To Buy Ingredients</h3>
+<p className="el-descr">sdfsdfsdfsdf</p>
+</div>
+      </div>
+
+      <div className="element">
+<div>
+<h3 className="el-title">Add recipe</h3>
+<p className="el-descr">sdfsdfsdfsdf</p>
+</div>
+      </div>
+
+    </section>
+
+    </div>
+  )
+}
+
+export default Services
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import About from "../components/About"
 import Video from "../components/Video"
+import Services from "../components/Services"
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
 {/*         <h1>Home Page</h1> */}
         <Video/>
         <About/>
+        <Services/>
     </div>
   )
 }
