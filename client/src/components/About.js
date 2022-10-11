dd85aab60d3697b4d57d406324dacdd8585f12b2
kateFrontend <ecdanilova@gmail.com>
2022-10-10 21:48:30 -0400

    about section style update

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,10 +1,16 @@
 import { LoremIpsum } from "react-lorem-ipsum";
+import image from './family.jpg'
 
 const About = () => {
   return (
     <section className='section about-section'>
-      <h2 className='section-title'>about us</h2>
+      <div>
+      <h1 className='about-title'>about us</h1>
       <p><LoremIpsum/></p>
+      </div>
+      <div>
+      <img className="img-about" src={image} alt="Family image"/>
+      </div>
     </section>
   )
 }
