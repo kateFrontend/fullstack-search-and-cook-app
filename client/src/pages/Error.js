6d13f17a169dda6b78506325cc3dde47dbcd869e
kateFrontend <ecdanilova@gmail.com>
2022-10-11 02:33:54 -0400

    error page update

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom'
+import image from './error.png'
+import styled from 'styled-components'
 
 const Error = () => {
   return (
     <section className='error-page section'>
       <div className='error-container'>
+        <Img src={image} alt='error'/>
         <h1>oops! nothing interesting here</h1>
         <Link to='/' className='btn btn-primary'>
             back home
@@ -12,5 +15,9 @@ const Error = () => {
     </section>
   )
 }
+
+const Img = styled.img`
+width: 500px;
+`
 
 export default Error
