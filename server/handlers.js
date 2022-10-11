39103086947374617dcb653ef18bdd2a21b430a6
kateFrontend <ecdanilova@gmail.com>
2022-10-10 20:29:44 -0400

    update server

--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const { uuid } = require('uuidv4');
+
+const bodyParser = require('body-parser')
+
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
@@ -23,13 +27,153 @@ const getRecipes = async (req, res) => {
 
         const result = await db 
         .collection("recipes").find({}).toArray();
-        res.status(200).json({ status: 200, data: result});
+
+
+        result
+        ? res.status(200).json({ status: 200, result, message: "Succes" })
+        : res.status(404).json({ status: 400, message: "Bad news" });
 
     } catch (err){
-        res.status(404).json({ status: 404, data: err });
+        res.status(500).json({ status: 500, data: req.body, message: err.message })
     }
     client.close();
     console.log("disconnected!");  
 }
 
-module.exports = { getRecipes };
+
+///////////////////////////////////////////////////////////////////////////////
+
+const createRecipe = async (req, res) => {
+    const client = new MongoClient(MONGO_URI, options);
+
+    try{
+        await client.connect();
+        console.log("connected")
+
+  const { name, description, ingredients, instructions } = req.body; 
+        
+
+        const db = client.db("SeachAndCook");
+
+        const _id = uuid();
+
+      const newRecipe = {
+            _id,
+            'name':name,
+            'description':description,
+            'ingredients':ingredients,
+            'instructions':instructions
+        }; 
+
+        const result = await db.collection("recipes").insertOne(newRecipe);
+
+        result
+        ? res.status(200).json({ status: 200, result, message: "Success" })
+        : res.status(404).json({ status: 400, message: "Bad news" });
+
+    } catch (err){
+        res.status(500).json({ status: 500, message: err.message });
+    }
+    client.close();
+    console.log("disconnected!");  
+}
+
+//////////////////////////////////////////////////////////////////
+
+
+const getRecipeById = async (req, res) => {
+
+    const client = new MongoClient(MONGO_URI, options);
+
+    try {
+        await client.connect();
+
+        const db = client.db("SeachAndCook");
+
+        const { _id } = req.params;
+        
+        const result = await db.collection("recipes").findOne({ _id });
+
+        result
+            ? res.status(200).json({ status: 200, result, message: "Success" })
+            : res.status(400).json({ status: 400, message: "Bad news" });
+    } 
+
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ status: 500, data: req.body, message: err.message })
+    }
+
+    finally {
+        client.close();
+    }
+};
+
+///////////////////////////////////////////////////
+
+const deleteRecipe = async (req, res) => {
+
+    const client = new MongoClient(MONGO_URI, options);
+    
+    try {
+        await client.connect();
+
+        const { _id } = req.params;
+
+        const db = client.db("SeachAndCook");
+
+        const result = await db.collection("recipes").deleteOne({ _id });
+
+        result
+            ? res.status(200).json({ status: 200, data: "Recipe deleted successfully" })
+            : res.status(404).json({ status: 404, data: "Recipe is not found" });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ status: 500, message: err.message });
+    }
+    finally {
+        client.close();
+    }
+};
+
+const updateRecipe = async (req, res) => {
+
+    const client = new MongoClient(MONGO_URI, options);
+
+    const { _id } = req.params;
+
+    try {
+
+        await client.connect();
+
+        const db = client.db("SeachAndCook");
+
+        const { name, description, ingredients, instructions } = req.body;
+
+        const query = { _id };
+
+        let newValues = { $set: { name: name, description: description, ingredients: ingredients, instructions: instructions } }
+
+        const result = await db.collection("recipes").updateOne(query, newValues);
+
+        result
+            ? res.status(200).json({ status: 200, data: req.body, message: "Recipe is updated" })
+            : res.status(404).json({ status: 404, data: req.body, message: "Recipe is not found" });
+    }
+
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ status: 500, data: req.body, message: err.message })
+    }
+
+    finally {
+        // TODO: close...
+        client.close();
+    }
+};
+
+
+
+
+module.exports = { getRecipes, createRecipe, getRecipeById, deleteRecipe, updateRecipe };
--- server/server.js
+++ server/server.js
@@ -2,20 +2,28 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-const { getRecipes } = require('./handlers')
+
+const { getRecipes, createRecipe, getRecipeById, deleteRecipe, updateRecipe } = require('./handlers')
 
 const port = 8000;
 
 express()
 
+    .use(express.urlencoded({ extended: true} ))
     .use(helmet())
     .use(morgan('tiny'))
+    
 
     .get("/hello", (req, res) => {
         res.status(200).json({ status: 200, message: "Hi!" })
     })
 
     .get("/api/addrecipe", getRecipes)
+    .post("/api/addrecipe", createRecipe)
+    .get("/api/addrecipe/:_id", getRecipeById)
+    .delete("/api/addrecipe/:_id", deleteRecipe)
+    .put("/api/addrecipe/:id", updateRecipe)
+
 
     .listen(8000, () => {
   console.log(`Server launched on port ${port}`)
