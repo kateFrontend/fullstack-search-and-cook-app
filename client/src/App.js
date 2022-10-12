6a20f046469bae03ac646d223e590fbff3e8eb41
kateFrontend <ecdanilova@gmail.com>
2022-10-12 19:47:54 -0400

    update project

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,6 @@ import Home from "./pages/Home";
 import Login from "./pages/Login"
 import Search from "./pages/Search";
 import SingleRecipe from "./pages/SingleRecipe";
-import Profile from "./pages/Profile";
-import ProfileRecipe from "./pages/ProfileRecipe";
 import AddRecipe from "./pages/AddRecipe"
 import Error from './pages/Error'
 import Navbar from "./components/Navbar";
@@ -27,13 +25,10 @@ const App = () => {
           <Route path="search" element={ <Search/> } />
           <Route path="search/:id" element={ <SingleRecipe/> } />
           <Route path="login" element={ <Login/> } />
-{/*           <Route path="profile" element={ <Profile /> } /> */}
           <Route path="buy" element={ <BuyAndCook/> } />
-{/*           <Route path="profile/:recipe" element={ <ProfileRecipe/> } /> */}
           <Route path="addrecipe" element={ <AddRecipe/> } />
           <Route path="*" element={ <Error/> } />
         </Routes>
-  {/*     <Footer/> */}
     </BrowserRouter>
     </FormspreeProvider>
     </AuthWrapper>
--- client/src/pages/AddRecipe.js
+++ client/src/pages/AddRecipe.js
@@ -1,19 +1,51 @@
-import React from 'react'
+import { useState } from 'react'
+
 import AddRecipeForm from '../components/AddRecipeForm'
 import styled from 'styled-components'
 
-function AddRecipe() {
+function AddRecipe(props) {
+
+  const [showForm, setShowForm] = useState(false)
+
+/*   function onCreateRecipe(recipe) {
+    props.AddRecipe(recipe)
+  } */
+
+ function onCreateNewRecipe() {
+  setShowForm(true);
+ }
+
   return (
     <div>
-      <Title>This section is not finished yet!Will be done in the few days!</Title>
-      <AddRecipeForm/></div>
+      <Title>Create&Cook</Title>
+{/*       <Title>This section is not finished yet!Will be done in the few days!</Title> */}
+{/*       <AddRecipeForm/> */}
+      {!showForm && <Button onClick={onCreateNewRecipe}>Create Recipe</Button>}
+      {showForm && <AddRecipeForm addRecipe={onCreateNewRecipe} />}
+      </div>
   )
 }
+
+
+
+
+
+
+
+
 
 const Title = styled.h1`
 display: flex;
 justify-content: center;
 margin-top: 10rem;
+`
+
+const Button = styled.button`
+display: flex;
+justify-content: center;
+align-items: center;
+margin: 10rem auto;
+padding: 2rem 10rem;
 `
 
 export default AddRecipe
