bff8633b806a3e75a23580c567964de80d7d37e6
kateFrontend <ecdanilova@gmail.com>
2022-10-16 16:57:04 -0400

    update form

--- client/src/pages/AddRecipev2.js
+++ client/src/pages/AddRecipev2.js
@@ -39,8 +39,9 @@ function AddRecipe(props) {
     return (
         <div>
             <Title>Create&Cook</Title>
-{/*             <SubTitle>Still in work!!!!!</SubTitle>
- */}            <Form onSubmit={handleCreateRecipe}>
+            {/*             <SubTitle>Still in work!!!!!</SubTitle>
+             */}{' '}
+            <Form onSubmit={handleCreateRecipe}>
                 <div>
                     <label htmlFor="name">Recipe Title:</label>
                     <input
@@ -57,7 +58,7 @@ function AddRecipe(props) {
 
                 <div>
                     <label htmlFor="ingredients">Ingredients:</label>
-                    <textarea id="ingredients" name="ingredients"></textarea>
+                    <textarea name="ingredients"></textarea>
                 </div>
 
                 <div className="form-group">
@@ -89,6 +90,19 @@ const Button = styled.button`
     align-items: center;
     margin: 2rem auto;
     padding: 1rem 10rem;
+    font-size: 1.2rem;
+    font-weight: bold;
+    cursor: pointer;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+    border-radius: 0.25rem;
+    border: none;
+    transition: all 0.3s linear;
+    background: var(--primaryColor);
+    color: #fff;
+    &:hover {
+        background: var(--primaryYellow);
+        color: var(--primaryColor);
+    }
 `
 
 const Form = styled.form`
@@ -98,14 +112,41 @@ const Form = styled.form`
     justify-content: center;
     margin: 0 auto;
 
+    label {
+        display: block;
+        margin: 1rem 0 0 0;
+        font-size: 1.5rem;
+        font-weight: bold;
+        letter-spacing: 0.25rem;
+        color: var(--primaryColor);
+    }
+
+    input {
+        border-radius: var(--mainBorderRadius);
+        padding: 0.5rem;
+        font-size: 1.2rem;
+        border: 1px solid var(--primaryColor);
+        border-bottom: 2px solid var(--primaryColor);
+        box-shadow: var(--lightShadow);
+    }
+
+    textarea {
+        border-radius: var(--mainBorderRadius);
+        padding: 0.5rem;
+        height: 100px;
+        font-size: 1.2rem;
+        border: 1px solid var(--primaryColor);
+        border-bottom: 2px solid var(--primaryColor);
+        box-shadow: var(--lightShadow);
+    }
 
 `
 
 const List = styled.div`
-display: flex;
-flex-wrap: wrap;
-justify-content: space-between;
-margin: 5rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin: 5rem;
 `
 
 export default AddRecipe
